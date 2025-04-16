066a56ece3cd18f41e67f037812d8db240d99e12
Myzoulz <lucasrocha7455@gmail.com>
2025-04-16 15:38:17 -0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function acionarQuestao01() {
   const numero = Number (document.getElementById("input01").value);
+  const retorno = document.getElementById("resultado01");
 
   if (numero % 2 === 0) {
-    alert("O número é par");
+    retorno.innerText = ("O número é par");
   }
   else {
-    alert("O número é ímpar");
+    retorno.innerText = ("O número é ímpar");
     return;
   }
 }
@@ -13,15 +14,16 @@ function acionarQuestao01() {
 function acionarQuestao02() {
   const numero1 = Number (document.getElementById("input02").value);
   const numero2 = Number (document.getElementById("input02-2").value);
+  const retorno = document.getElementById("resultado02");
 
   if (numero1 > numero2) {
-    alert(`${numero1} é maior que ${numero2}`);
+    retorno.innerText = (`${numero1} é maior que ${numero2}`);
   }
   else if (numero1 < numero2) {
-    alert(`${numero2} é maior que ${numero1}`);
+    retorno.innerText = (`${numero2} é maior que ${numero1}`);
   }
   else {
-    alert("Os números são iguais");
+    retorno.innerText = ("Os números são iguais");
     return;
   }
 }
@@ -32,17 +34,18 @@ function acionarQuestao03() {
   const anoInserido = new Date(data).getFullYear();
   const anoAtual = dataAtual.getFullYear();
   const idade = anoAtual - anoInserido;
+  const retorno = document.getElementById("resultado03");
 
   if (idade > 150 || idade < 0) {
-    alert("Idade inválida");
+    retorno.innerText = ("Idade inválida");
     return;
   }
 
   if (idade >= 16) {
-    alert(`A pessoa tem ${idade} e pode votar`);
+    retorno.innerText = (`A pessoa tem ${idade} anos e pode votar`);
   }
   else {
-    alert(`A pessoa tem ${idade} e não pode votar`);
+    retorno.innerText = (`A pessoa tem ${idade} anos e não pode votar`);
     return;
   }
 }
@@ -52,53 +55,56 @@ function acionarQuestao04() {
   const reajuste = Number (document.getElementById("input04-2").value);
   const aumento = (salario * reajuste) / 100;
   const novoSalario = salario + aumento;
+  const retorno = document.getElementById("resultado04");
 
   if (novoSalario < 0) {
-    alert("Salario inválido");
+    retorno.innerText = ("Salario inválido");
     return;
   }
 
-  alert(`O novo salário é R$ ${novoSalario.toFixed(2)}`);
+  retorno.innerText = (`O novo salário é R$ ${novoSalario.toFixed(2)}`);
 }
 
 function acionarQuestao05() {
   const dia = Number (document.getElementById("input05").value);
+  const retorno = document.getElementById("resultado05");
 
   switch (dia) {
     case 1:
-      alert("Domingo");
+      retorno.innerText = ("Domingo");
       break;
     case 2:
-      alert("Segunda-feira");
+      retorno.innerText = ("Segunda-feira");
       break;
     case 3:
-      alert("Terça-feira");
+      retorno.innerText = ("Terça-feira");
       break;
     case 4:
-      alert("Quarta-feira");
+      retorno.innerText = ("Quarta-feira");
       break;
     case 5:
-      alert("Quinta-feira");
+      retorno.innerText = ("Quinta-feira");
       break;
     case 6:
-      alert("Sexta-feira");
+      retorno.innerText = ("Sexta-feira");
       break;
     case 7:
-      alert("Sábado");
+      retorno.innerText = ("Sábado");
       break;
     default:
-      alert("Dia inválido");
+      retorno.innerText = ("Dia inválido");
       return;
   }
 }
 
 function acionarQuestao06() {
   const numero = Number (document.getElementById("input06").value);
+  const retorno = document.getElementById("resultado06");
   let contador = 0;
   let resultado = [];
 
   if (numero < 0) {
-    alert("Número inválido");
+    retorno.innerText = ("Número inválido");
     return;
   }
 
@@ -107,15 +113,16 @@ function acionarQuestao06() {
     contador++;
   }
 
-  alert(`${resultado.join(", ")},`);
+  retorno.innerText = (`${resultado.join(", ")},`);
 }
 
 function acionarQuestao07() {
   const numero = Number (document.getElementById("input07").value);
+  const retorno = document.getElementById("resultado07");
   let soma = 0;
 
   if (numero < 0) {
-    alert("Número inválido");
+    retorno.innerText = ("Número inválido");
     return;
   }
 
@@ -123,12 +130,13 @@ function acionarQuestao07() {
     soma += i;
   }
 
-  alert(`A soma de todos os números de 0 até ${numero} é: ${soma}`);
+  retorno.innerText = (`A soma de todos os números de 0 até ${numero} é: ${soma}`);
 
 }
 
 function acionarQuestao08() {
   const numero = Number (document.getElementById("input08").value);
+  const retorno = document.getElementById("resultado08");
   let listaImpar = [];
 
   for (let i = 1; i < numero; i++) {
@@ -137,19 +145,19 @@ function acionarQuestao08() {
     }
   }
   if (listaImpar.length === 0) {
-    alert("Não há números ímpares");
+    retorno.innerText = ("Não há números ímpares");
     return;
   }
-  alert(`Os números ímpares são: ${listaImpar.join(", ")}`);
+  retorno.innerText = (`Os números ímpares são: ${listaImpar.join(", ")}`);
 }
 
 function acionarQuestao09() {
   const numerosString = document.getElementById("input09").value;
-
+  const retorno = document.getElementById("resultado09");
   const numeros = numerosString.split("-").map(Number);
 
   if (numeros.some(numero => !Number.isInteger(numero))) {
-    alert("Todos os números devem ser inteiros.");
+    retorno.innerText = ("Todos os números devem ser inteiros.");
     return;
   }
 
@@ -157,45 +165,52 @@ function acionarQuestao09() {
     .filter(numero => numero < 30)
     .reduce((acumulador, numero) => acumulador + numero, 0);
 
-  alert(`A soma dos números menores que 30 é: ${soma}`);
+    retorno.innerText = (`A soma dos números menores que 30 é: ${soma}`);
 }
 
 function acionarQuestao10() {
   const numerosString = document.getElementById("input10").value;
-
   const numeros = numerosString.split("/").map(Number);
+  const retorno = document.getElementById("resultado10");
 
   if (numeros.some(numero => !Number.isInteger(numero))) {
-    alert("Todos os números devem ser inteiros.");
+    retorno.innerText = ("Todos os números devem ser inteiros.");
     return;
   }
 
   const maioresQue50 = numeros.filter(numero => numero > 50);
 
-  alert(`A quantidade de números maiores que 50 é: ${maioresQue50.length}`);
+  retorno.innerText = (`A quantidade de números maiores que 50 é: ${maioresQue50.length}`);
 }
 
 function acionarQuestao11() {
   const string = document.getElementById("input11").value;
+  const retorno = document.getElementById("resultado11");
   let stringInvertida = "";
+
+  if (string.length === 0) {
+    retorno.innerText = ("A string não pode estar vazia.");
+    return;
+  }
 
   for (let i = string.length - 1; i >= 0; i--) {
     stringInvertida += string[i];
   }
 
-  alert(`A string invertida é: ${stringInvertida}`);
+  retorno.innerText = (`A string invertida é: ${stringInvertida}`);
 }
 
 function acionarQuestao12() {
   const string = document.getElementById("input12").value;
+  const retorno = document.getElementById("resultado12");
 
   if (string.length === 0) {
-    alert("A string não pode estar vazia.");
+    retorno.innerText = ("A string não pode estar vazia.");
     return;
   }
 
   if (string.length === 1) {
-    alert(`Resultado: ${string.toUpperCase()}`);
+    retorno.innerText = (`Resultado: ${string.toUpperCase()}`);
     return;
   }
 
@@ -204,8 +219,9 @@ function acionarQuestao12() {
     string.slice(1, -1) +
     string[string.length - 1].toUpperCase();
 
-  alert(`Resultado: ${resultado}`);
+    retorno.innerText = (`Resultado: ${resultado}`);
 }
+
 
 
 
